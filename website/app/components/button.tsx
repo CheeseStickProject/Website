d3934ce1a0b6f53c8eed26146b7bfba9edc7d2fe
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-01-18 21:46:56 -0600

    added in next.js with react

---
+++ website/app/components/button.tsx
@@ -0,0 +1,9 @@
+export function TestButton() {
+  return (
+    <div>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded">
+        Button
+      </button>
+    </div>
+  );
+}
---
+++ website/app/page.tsx
@@ -0,0 +1,12 @@
+import { TestButton} from './components/button';
+
+
+
+export default function Home() {
+  return (
+    <main className="items-center allign-center min-h-screen bg-[var(--background)] text-[var(--foreground)]">
+      <h1>Welcome to My Website</h1>
+        <TestButton />
+    </main>
+  );
+}
